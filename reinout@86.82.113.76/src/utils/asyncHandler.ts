3f9c69405be01bd6be71bfed89e6ae4387da9503
sensei <78255922+zee-egel@users.noreply.github.com>
2025-09-21 23:03:43 +0200

    Delete directory

--- reinout@86.82.113.76/src/utils/asyncHandler.ts
+++
@@ -1,7 +0,0 @@
-import type { RequestHandler } from "express";
-
-export const asyncHandler = (handler: RequestHandler): RequestHandler => {
-  return (req, res, next) => {
-    Promise.resolve(handler(req, res, next)).catch(next);
-  };
-};
